d2eddf0d455b548bd036bc11f222680b3ce8186d
OmerFarukIcli <omerfarukiclikku@gmail.com>
2021-08-30 00:43:56 +0300

    Okul versiyon 1

---
+++ src/okulYonetimi/Main.java
@@ -0,0 +1,14 @@
+package okulYonetimi;
+
+import okulYonetimi.Islemler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Islemler islem = new Islemler();
+        islem.testKisiOlustur();
+        islem.anaMenu();
+
+	}
+
+}
